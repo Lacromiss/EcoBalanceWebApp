ff851efbe3b1c924c548871172af8810410b178f
Lacromisss <user.email>
2022-12-06 02:36:23 +0400

    Part1

---
+++ Lacromis/DAL/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Lacromis.DAL
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
